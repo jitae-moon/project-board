002a42988c23a1dea3325cff1843911718bcaa12
jitae-moon <jitae.moon@icloud.com>
2024-06-04 15:18:52 +0900

    #10 - 도메인 코드를 엔티티로 변환
    * 1 도메인 클래스 == 1 테이블이 되도록 매칭
    * 생성자와 'equals()', 'hashCode()'등 override

--- src/main/java/com/example/projectboard/domain/Article.java
+++ src/main/java/com/example/projectboard/domain/Article.java
@@ -1,15 +1,76 @@
 package com.example.projectboard.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity // 기본 생성자 있어야 함(hibernate 기준)
 public class Article {
-    private Long id;
-    private String title; // 제목
-    private String content; // 내용
-    private String hashtag; // 해시태그
 
-    private LocalDateTime createdAt; // 생성일시
-    private String createdBy; // 생성자
-    private LocalDateTime modifiedAt; // 수정일시
-    private String modifiedBy; // 수정자
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // MySQL은 Identity 방식으로 Auto-increment 됨
+    private Long id;
+
+    // Setter를 클래스 레벨에 적용할 경우 id도 바뀔 수가 있음
+    @Setter @Column(nullable = false) private String title; // 제목
+    @Setter @Column(nullable = false, length = 10000) private String content; // 내용
+    @Setter private String hashtag; // 해시태그
+
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL) // Article table로부터 왔다는 것 명시
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
+
+    protected Article() {}
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    // Factory method pattern
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    // ID만 검사하면 동일한지 알 수 있음
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id); // DB에 아직 저장 안 되었을 때는 id가 null일 수 있음
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
+
+
