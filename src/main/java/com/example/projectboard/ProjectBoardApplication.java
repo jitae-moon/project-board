2f01a80d6f33f8689820476bb7f9b3c93faeb1c7
jitae-moon <jitae.moon@icloud.com>
2024-06-05 14:33:00 +0900

    #16 - 타임리프 decoupled logic 추가
    th 속성 분리를 위해 decoupled logic 추가.
    
    * https://gist.github.com/djkeh/6e1d557ce8c466135b1541d342b1c25c

--- src/main/java/com/example/projectboard/ProjectBoardApplication.java
+++ src/main/java/com/example/projectboard/ProjectBoardApplication.java
@@ -2,7 +2,9 @@ package com.example.projectboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan // ThymeleafConfig 관련
 @SpringBootApplication
 public class ProjectBoardApplication {
 
