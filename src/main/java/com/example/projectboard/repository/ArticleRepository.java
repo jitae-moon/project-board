790b64e2e217feb6c1ca3ed0b7826e451526794b
jitae-moon <jitae.moon@icloud.com>
2024-06-05 10:43:42 +0900

    #1 - Querydsl을 통해 검색 옵션 추가
    'QuerydslPredicateExecutor', 'QuerydslBinderCustomizer' 활용해서 검색 기능 추가.

--- src/main/java/com/example/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/example/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,33 @@
 package com.example.projectboard.repository;
 
 import com.example.projectboard.domain.Article;
+import com.example.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>, // Article안에 있는 필드들에 대한 기본 검색 기능
+        QuerydslBinderCustomizer<QArticle>
+{
+    // 검색에 대한 세부적인 규칙
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true); // 필드들 선택적으로 검색하도록
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        bindings.bind(root.title)
+                .first(StringExpression::containsIgnoreCase); // 검색 파라미터 한 개 LIKE '%${v}%'
+//        bindings.bind(root.title)
+//                .first(StringExpression::likeIgnoreCase); // LIKE '${v}'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
