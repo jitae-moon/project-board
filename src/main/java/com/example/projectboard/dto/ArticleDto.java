daf12647db1d50893ed9b12abb5e8275ad295205
jitae-moon <jitae.moon@icloud.com>
2024-06-07 15:04:04 +0900

    #16 - 게시글 비지니스 로직 추가
    게시글 비지니스 로직 및 관련 DTO 추가.
    테스트는 단위 테스트(solitary test)를 위해 Mockito 사용.

---
+++ src/main/java/com/example/projectboard/dto/ArticleDto.java
@@ -0,0 +1,15 @@
+package com.example.projectboard.dto;
+
+import java.time.LocalDateTime;
+
+public record ArticleDto(
+        String title,
+        String content,
+        String hashtag,
+        LocalDateTime createdAt,
+        String createdBy
+) {
+    public static ArticleDto of(String title, String content, String hashtag, LocalDateTime createdAt, String createdBy) {
+        return new ArticleDto(title, content, hashtag, createdAt, createdBy);
+    }
+}
